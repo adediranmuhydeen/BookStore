06564758c9ad18514e05100e03ce9294a1762430
Muhydeen Adebayo <adediranma@examroom.ai>
2023-06-23 18:14:31 +0100

    Input validation modified

--- BookStore.Web/Models/Category.cs
+++ BookStore.Web/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BookStore.Web.Models
@@ -7,10 +8,11 @@ namespace BookStore.Web.Models
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Name is required")]
         public string? Name { get; set; }
-        [Required]
+        [Required(ErrorMessage ="Quantity is required"), DisplayName("Quantity")]
         public int DiplayOrder { get; set; }
+        [Required(ErrorMessage ="Date is required"), DisplayName("Date")]
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
     }
 }
